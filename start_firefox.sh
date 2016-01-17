bd81e97585be76984e101a7f6aa21c5c2bae01ab
Stenver Jerkku <stenver1010@gmail.com>
2016-01-18 01:34:20 +0200

    Fix injecting environmental variables

--- start_firefox.sh
+++ start_firefox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 JAVA_OPTS="-Xmx512m -Dwebdriver.server.session.timeout=-1 -Dwebdriver.server.browser.timeout=-1"
-fox=`docker run -d -P -e JAVA_OPTS=$JAVA_OPTS --link selenium-hub:hub selenium/node-firefox-debug`
+fox=`docker run -d -P -e JAVA_OPTS="$JAVA_OPTS" --link selenium-hub:hub selenium/node-firefox-debug`
 echo $fox
 docker port $fox
