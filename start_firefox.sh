52082626782e0365fb97a607cc88ef45e7efceba
Stenver Jerkku <stenver1010@gmail.com>
2016-01-18 01:22:47 +0200

    Experiment java options for node timeout

--- start_firefox.sh
+++ start_firefox.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-fox=`docker run -d -P --link selenium-hub:hub selenium/node-firefox-debug`
+JAVA_OPTS="-Xmx512m -Dwebdriver.server.session.timeout=-1 -Dwebdriver.server.browser.timeout=-1"
+fox=`docker run -d -P -e JAVA_OPTS=$JAVA_OPTS --link selenium-hub:hub selenium/node-firefox-debug`
 echo $fox
 docker port $fox
--- start_hub.sh
+++ start_hub.sh
@@ -5,5 +5,6 @@ docker kill $(docker ps -a | awk '{print $1}') || true
 docker rm $(docker ps -a | awk '{print $1}') || true
 
 #Start hub
-docker run -d -p 4444:4444 -e JAVA_OPTS=-Xmx512m -e GRID_TIMEOUT=86400 -e GRID_BROWSER_TIMEOUT=86400 --name selenium-hub selenium/hub
+JAVA_OPTS="-Xmx512m -Dwebdriver.server.session.timeout=-1 -Dwebdriver.server.browser.timeout=-1"
+docker run -d -p 4444:4444 -e JAVA_OPTS=$JAVA_OPTS -e GRID_TIMEOUT=86400 -e GRID_BROWSER_TIMEOUT=86400 --name selenium-hub selenium/hub
 
